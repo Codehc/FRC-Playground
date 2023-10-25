f241b3d61db9d7b19558fcd5613a7409af791b6e
Codehc <57022309+Codehc@users.noreply.github.com>
2023-10-25 11:44:24 -0700

    Revert "Swerve sim :eyes:. Simulated NEO class by adding to the built in REV swerve utils. Should be generic so it should be p easy to throw onto other subsystems."
    This reverts commit 70979e8e25562232f29c6ee90e96270d528dba36.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,7 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.REVPhysicsSim;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -19,18 +9,11 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
   private Command m_autonomousCommand;
 
-  private NEO neo = new NEO(0);
-
-  private SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-
-  private PS4Controller controller = new PS4Controller(0);
-  private PS4Controller controller2 = new PS4Controller(1);
-
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
 
-    DataLogManager.start("./sim-logs");
+    DataLogManager.start();
 
     CommandScheduler.getInstance().onCommandInitialize((Command command) -> {
       DataLogManager.log(command.getName() + " is initializing and requires: " + command.getRequirements().toString());
@@ -43,17 +26,9 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().onCommandFinish((Command command) -> {
       DataLogManager.log(command.getName() + " is finishing on its own.");
     });
-
-    SmartDashboard.putNumber("Input Voltage", 0);
-    SmartDashboard.putNumber("Target Position", 0);
-    SmartDashboard.putNumber("Times updated", 0);
-
-    //neo.encoder.getVelocity()
-
-    neo.pidController.setP(0.01);
   }
 
-  @Override
+  @Override 
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
@@ -61,8 +36,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-
+    
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
@@ -70,8 +44,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {
-  }
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
@@ -83,30 +56,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    /*if (neo.getTargetVelocity() != SmartDashboard.getNumber("Target Position", 0)) {
-      SmartDashboard.putNumber("Times updated", SmartDashboard.getNumber("Times updated", 0) + 1);
-      neo.setTargetVelocity(SmartDashboard.getNumber("Target Position", 0));
-    }
-
-    SpyderRobot.tick();
-
-    SmartDashboard.putNumber("Rotations", neo.getPosition());
-    SmartDashboard.putNumber("Velocity", neo.getVelocity());
-    SmartDashboard.putNumber("Applied Output", neo.getAppliedOutput());*/
-
-    double leftY = -3 * controller.getLeftY();
-    double leftX = -3 * controller.getLeftX();
-    double rightX = -1.4 * controller.getRightX();
-
-    leftY = Utils.deadband(leftY, 0.2);
-    leftX = Utils.deadband(leftX, 0.2);
-    rightX = Utils.deadband(rightX, 0.1);
-
-    swerveSubsystem.driveFieldRelative(leftY, leftX, rightX);
-
-    swerveSubsystem.periodic();
-
-    SpyderRobot.tick();
   }
 
   @Override
@@ -115,8 +64,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledPeriodic() {
-  }
+  public void disabledPeriodic() {}
 
   @Override
   public void testInit() {
@@ -125,15 +73,11 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {
-  }
+  public void testPeriodic() {}
 
   @Override
-  public void simulationInit() {
-  }
+  public void simulationInit() {}
 
   @Override
-  public void simulationPeriodic() {
-    SpyderRobot.simulationTick();
-  }
+  public void simulationPeriodic() {}
 }
